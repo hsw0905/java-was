4b540c768b59139c2f84ae9b13028ef9cbf37242
hsw0905 <hsw0905@gmail.com>
2022-12-15 17:14:21 +0900

    add logger

--- src/main/java/study/web/Server.java
+++ src/main/java/study/web/Server.java
@@ -3,16 +3,21 @@ package study.web;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Server {
 
   private final int PORT = 8080;
+  private static final Logger logger = LoggerFactory.getLogger(Server.class);
 
   public void run() {
     try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-      Socket connection;
-      while ((connection = serverSocket.accept())!=null) {
-        System.out.println("connected");
+      logger.info("WebServer Started. waiting for client...");
+
+      Socket clientSocket;
+      while ((clientSocket = serverSocket.accept())!=null) {
+        logger.info("{} connected", clientSocket.getInetAddress());
 
       }
     } catch (IOException e) {
